391fb1e87fa50b70febab51a284110db6f679fea
WojtekCodesToday <94251112+WojtekCodesToday@users.noreply.github.com>
2022-01-29 23:33:32 +0000

    Version 1.1
    1.1

--- script.js
+++ script.js
@@ -1,6 +1,28 @@
 // Make the DIV element draggable:
 dragElement(document.getElementById("mydiv"));
 
+let taskbar = document.getElementsByClassName("taskbar")[0]
+let startmenu = document.getElementsByClassName("startmenu")[0]
+   
+taskbar.addEventListener("click", ()=>{
+    console.log("clicked");
+    if(startmenu.style.bottom == "50px"){
+        startmenu.style.bottom = "-655px"
+    }
+    else{
+        startmenu.style.bottom = "50px"
+    }
+})
+
+function myFunction() {
+    var x = document.getElementById("myDIV");
+    if (x.style.display === "none") {
+      x.style.display = "block";
+    } else {
+      x.style.display = "none";
+    }
+  }
+
 function dragElement(elmnt) {
   var pos1 = 0, pos2 = 0, pos3 = 0, pos4 = 0;
   if (document.getElementById(elmnt.id + "header")) {
